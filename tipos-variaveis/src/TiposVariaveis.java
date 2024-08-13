fd3a42f9e9a8101d15e23ddc70d82a66c87925f1
DenisVini <denisvini@gmail.com>
2024-08-13 11:56:50 -0300

    Adicionando diretórios da aula sobre váriaveis.

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class TiposVariaveis {
+    public static void main(String[] args) {
+             Scanner leia = new Scanner(System.in);
+                
+             int number;
+            
+        System.out.print("What is the maximum number for a byte variable?: ");
+		number = leia.nextInt();
+        
+        if(number>127){
+        }else if (number<-128) {  
+        System.out.println("This number represents at least one short.");}
+        else {System.out.println("This number represents at byte.");
+             }
+             
+        
     }
 }
